bfb1fcd4c5f483aa3bb6a6856712c95d9e17bfab
Hamoud <skhamoud@gmail.com>
2017-06-13 12:54:26 +0000

    Implement Api data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Users from "./components/users";
+import { getUsersData, getUserStream } from "./api";
+
+import "noty/lib/noty.css";
+import "bulma/css/bulma.css";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usersData: [],
+      loading: false
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    getUsersData().then(res => {
+      const newUsers = [];
+      return res.data.users.map(user => {
+        let newUserData = {};
+        return getUserStream(user._id).then(res => {
+          newUserData = Object.assign({}, user, res.data);
+          newUsers.push(newUserData);
+          this.setState({ usersData: newUsers, loading: false });
+        });
+      });
+    });
+    // getStreams().then(result => console.log(result.data));
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Users users={this.state.usersData} />
       </div>
     );
   }
