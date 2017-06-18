38fff62c89ba79b941ba23d953afc3b509f878a2
Hamoud <skhamoud@gmail.com>
2017-06-18 01:35:20 +0000

    Added filtering feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Streamers from "./components/Streamers";
+import Header from "./components/Header";
 import { fetchData } from "./api";
 
 import "bulma/css/bulma.css";
@@ -10,7 +11,8 @@ class App extends Component {
     super(props);
     this.state = {
       streamsData: [],
-      loading: false
+      loading: false,
+      filter: "all"
     };
   }
   componentDidMount() {
@@ -24,12 +26,21 @@ class App extends Component {
     // TODO: Abstract Api calls with Promise.all ?
     // setting state every time is dumb
   }
-
+  filterData(filter) {
+    this.setState({ filter });
+  }
   render() {
     console.log(this.state);
     return (
       <div className="App">
-        <Streamers streamers={this.state.streamsData} />
+        <div className="container ">
+          <div className="columns" style={{ paddingTop: "40px" }}>
+            <div className="column is-offset-2 is-6">
+              <Header filterData={this.filterData.bind(this)} />
+              <Streamers {...this.state} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Streamers.js
+++ src/components/Streamers.js
@@ -1,39 +1,41 @@
 import React from "react";
 
-export default ({ streamers = [] }) => {
-  const StreamersList = streamers.map(streamer => {
-    return (
-      <li key={streamer._id} className="media stream">
-        <div className=" media-left">
-          <figure className=" image is-64x64">
-            <img src={streamer.logo} alt={streamer.name} />
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content ">
-            <p className="title is-4">{streamer.name}</p>
-            <p>{streamer.bio}</p>
+export default ({ streamsData, filter }) => {
+  let StreamersList = streamsData
+    // Filter through filter word
+    .filter(s => {
+      if (filter !== "all") return s.status === filter;
+      return s;
+    })
+    // map and return relevant ui
+    .map(streamer => {
+      return (
+        <li key={streamer._id} className="media stream">
+          <div className=" media-left">
+            <figure className=" image is-64x64">
+              <img src={streamer.logo} alt={streamer.name} />
+            </figure>
           </div>
-        </div>
-        <div className="stream__status">
-          {streamer.stream
-            ? <a href={streamer.stream.channel.url} target="_blank">
-                <span><i className="stream__status-icon online" />Online</span>
-              </a>
-            : <span><i className="stream__status-icon offline" />Offline</span>}
-        </div>
-      </li>
-    );
-  });
+          <div className="media-content">
+            <div className="content ">
+              <p className="title is-4">{streamer.name}</p>
+              <p>{streamer.bio}</p>
+            </div>
+          </div>
+          <div className="stream__status">
+            {streamer.status === "online"
+              ? <a href={streamer.url} target="_blank">
+                  <span><i className="stream__status-icon online" />Online</span>
+                </a>
+              : <span><i className="stream__status-icon offline" />Offline</span>}
+          </div>
+        </li>
+      );
+    });
 
   return (
-    <div className="container ">
-      <div className="columns" style={{ paddingTop: "40px" }}>
-        <ul className="column is-offset-2 is-6">
-          {StreamersList}
-        </ul>
-
-      </div>
-    </div>
+    <ul>
+      {StreamersList}
+    </ul>
   );
 };
