e504351a5b4bbd6571ec86d7e073f442e1339ba6
Hamoud S. Khatar <Hamoud S. Khatar>
2017-06-22 02:06:58 +0000

    Fix Loading Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Streamers from "./components/Streamers";
 import Header from "./components/Header";
+import LoadingModal from "./components/LoadingModal";
 import { fetchData } from "./api";
 
 import "bulma/css/bulma.css";
@@ -19,7 +20,7 @@ class App extends Component {
     this.setState({ loading: true });
     fetchData().then(res => {
       Promise.all([...res]).then(res => {
-        this.setState({ streamsData: res });
+        this.setState({ streamsData: res, loading: false });
       });
     });
   }
@@ -34,10 +35,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container ">
-          <div className="columns" style={{ paddingTop: "40px" }}>
+          <div className="columns">
             <div className="column is-offset-2 is-6">
-              <Header filterData={this.filterData.bind(this)} />
-              <Streamers {...this.state} />
+              {this.state.loading
+                ? <LoadingModal />
+                : <div>
+                    <Header filterData={this.filterData.bind(this)} />
+                    <Streamers {...this.state} />
+                  </div>}
             </div>
           </div>
         </div>
--- src/components/loadingModal.js
+++ src/components/loadingModal.js
@@ -1,11 +1,28 @@
+import React from "react";
 /**
- * LoadingModal for Ajax calls and such operations
+ * LoadingModal for Ajax calls and such operations - 
+ * Uses bulma.css
  */
 export default () =>
   <div className="modal is-active">
     <div className="modal-background" />
     <div className="modal-content has-text-centered">
-      <div className="button is-danger is-bold is-loading is-medium" />
+      <div className="button   is-loading is-large" />
+      <style>
+        {`.is-loading {
+          background:none;
+          border:none;
+          height:5em;
+        }
+        .is-loading.button::after{
+          width: 2em;
+          height:2em;
+          border:3px solid #68c9c0; 
+          border-right-color:transparent;
+          border-bottom-color:transparent;
+          animation: spinAround 900ms infinite linear;
+        }`}
+      </style>
     </div>
     <button className="modal-close" />
   </div>;
