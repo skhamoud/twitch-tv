a83cc6722f09ee2a40ed6f3483394235757ba0a7
Hamoud S. Khatar <Hamoud S. Khatar>
2017-06-29 15:18:32 +0000

    Add Streamer component and online transition styles

--- src/components/Streamers.js
+++ src/components/Streamers.js
@@ -1,56 +1,15 @@
 import React from "react";
-
+import Streamer from "./Streamer";
 export default ({ streamsData, filter }) => {
+  // Filter through filter word
   let StreamersList = streamsData
-    // Filter through filter word
     .filter(s => {
       if (filter !== "all") return s.status === filter;
       return s;
     })
     // map and return relevant ui
     .map(streamer => {
-      return (
-        <li key={streamer._id} className="media stream">
-          <div className=" media-left">
-            <figure className="image is-64x64">
-              <img
-                src={streamer.logo ? streamer.logo : "https://via.placeholder.com/64x64"}
-                alt={streamer.name}
-              />
-            </figure>
-          </div>
-          <div className="media-content">
-            <div className="content ">
-              <p className="title is-4">{streamer.name}</p>
-              <p>{streamer.bio ? streamer.bio : "No bio saved!"}</p>
-
-              {streamer.status === "online"
-                ? <div>
-                    <p><strong>Game: </strong>{streamer.game}</p>
-                    <p><strong>Language: </strong>{streamer.language} </p>
-                    <p><strong>Status: </strong>{streamer.currentStatus}</p>
-                    <p><strong>Viewers: </strong>{streamer.viewers} watching now!</p>
-                    <p><strong>Followers: </strong>{streamer.followers} follower</p>
-
-                    <p>
-                      <a href={streamer.url} target="_blank">
-                        <img src={streamer.mediumPreview} alt="" />
-                      </a>
-                    </p>
-
-                  </div>
-                : null}
-            </div>
-          </div>
-          <div className="stream__status">
-            {streamer.status === "online"
-              ? <a href={streamer.url} target="_blank">
-                  <span><i className="stream__status-icon online" />Online</span>
-                </a>
-              : <span><i className="stream__status-icon offline" />Offline</span>}
-          </div>
-        </li>
-      );
+      return <Streamer key={streamer._id} {...streamer} />;
     });
 
   return (
